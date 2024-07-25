aaf7b7682e0ff3850e9d49db44172a2e07f8a382
Maren van Otterdijk <maren.van.otterdijk@gmail.com>
2024-07-25 15:26:46 +0200

    depend on https://github.com/NixOS/nixpkgs/pull/329891 temporarily

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "SWI-Prolog versions";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:matko/nixpkgs/swipl_to_9.2.6";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
